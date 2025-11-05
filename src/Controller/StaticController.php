46e84a57f1b6ed67805f6e4b7d1c0d2d293daedf
miyuki <mailtomiyuko@gmail.com>
2025-11-05 16:17:17 +0100

    footer update

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -8,23 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class StaticController extends AbstractController
 {
-    #[Route('/tosho-home', name: 'tosho_home')]
-     public function index(
-       
-    ): Response {
-        return $this->render('static/privacy.html.twig');
+    #[Route('/', name: 'tosho_home')]
+    public function index(): Response
+    {
+        return $this->render('static/home.html.twig');
     }
     #[Route('/privacy', name: 'privacy')]
-     public function privacy(
-       
-    ): Response {
+    public function privacy(): Response
+    {
         return $this->render('static/privacy.html.twig');
     }
     #[Route('/mentions', name: 'mentions')]
-     public function mentions(
-       
-    ): Response {
+    public function mentions(): Response
+    {
         return $this->render('static/mentions.html.twig');
     }
-
+    #[Route('/cookies', name: 'cookies')]
+    public function cookies(): Response
+    {
+        return $this->render('static/cookies.html.twig');
+    }
 }
